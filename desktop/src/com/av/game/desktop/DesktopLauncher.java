19bfa6f6548a3e3ea332aa8c357e1dcf2187f9c8
VascoUP <vasco_mag_pereira@hotmail.com>
2017-08-07 10:39:02 +0100

    Project created

---
+++ desktop/src/com/av/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.av.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.av.game.RocketGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RocketGame(), config);
+	}
+}
