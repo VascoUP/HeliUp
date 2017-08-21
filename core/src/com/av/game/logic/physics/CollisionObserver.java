25e23fb3d25bbd7916a6f4c01c10cd80a9aac3c9
VascoUP <vasco_mag_pereira@hotmail.com>
2017-08-21 14:10:10 +0100

    Improved data structures

--- core/src/com/av/game/logic/physics/CollisionObserver.java
+++ core/src/com/av/game/logic/physics/CollisionObserver.java
@@ -1,10 +1,10 @@
 package com.av.game.logic.physics;
 
-import com.av.game.logic.Game;
 import com.av.game.logic.object.GameObject;
-import com.badlogic.gdx.Gdx;
 
 import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
 
 public class CollisionObserver {
@@ -14,14 +14,14 @@ public class CollisionObserver {
     private Set<GameObject> collision_objects;
     private Set<Collidable> collidables;
 
-    private Set<GameObject> collisions_to_remove;
-    private Set<Collidable> collidables_to_remove;
+    private Queue<GameObject> collisions_to_remove;
+    private Queue<Collidable> collidables_to_remove;
 
     private CollisionObserver() {
         collision_objects = new HashSet<GameObject>();
         collidables = new HashSet<Collidable>();
-        collisions_to_remove = new HashSet<GameObject>();
-        collidables_to_remove = new HashSet<Collidable>();
+        collisions_to_remove = new PriorityQueue<GameObject>();
+        collidables_to_remove = new PriorityQueue<Collidable>();
     }
 
     public static void createInstance() {
@@ -38,7 +38,6 @@ public class CollisionObserver {
 
     public static void removeCollisionObject(GameObject collision_object) {
         instance.collisions_to_remove.add(collision_object);
-        //instance.collision_objects.remove(collision_object);
     }
 
     public static void addCollidable(Collidable collidable) {
@@ -47,7 +46,6 @@ public class CollisionObserver {
 
     public static void removeCollidable(Collidable collidable) {
         instance.collidables_to_remove.add(collidable);
-        //instance.collidables.remove(collidable);
     }
 
     public static void clear() {
@@ -55,10 +53,10 @@ public class CollisionObserver {
     }
 
     public void checkCollisions() {
-        for(GameObject removable : collisions_to_remove)
-            collision_objects.remove(removable);
-        for(Collidable removable : collidables_to_remove)
-            collidables.remove(removable);
+        while(!collisions_to_remove.isEmpty())
+            collision_objects.remove(collisions_to_remove.remove());
+        while(!collidables_to_remove.isEmpty())
+            collidables.remove(collidables_to_remove.remove());
         for(GameObject collision_object : collision_objects)
             for(Collidable collidable : collidables)
                 if(collidable.isColliding(collision_object))
