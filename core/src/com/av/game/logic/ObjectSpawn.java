9ebdb49753809272f709ac08c6f15c60c726433e
VascoUP <vasco_mag_pereira@hotmail.com>
2017-08-23 20:37:42 +0100

    Balancing game (harder bches)

--- core/src/com/av/game/logic/ObjectSpawn.java
+++ core/src/com/av/game/logic/ObjectSpawn.java
@@ -14,22 +14,24 @@ import java.util.Random;
 public class ObjectSpawn {
     private static String TAG = "ObjectSpawn";
 
-    private static final float FUEL_SPAWN_TIME = 2f;
-    private static final float BUILDING_SPAWN_TIME = 2f;
+    private static final float FUEL_SPAWN_TIME = 3f;
+    private static final float BUILDING_SPAWN_TIME = 3f;
     private static float FUEL_MAX_HEIGHT;
     private static float BUILDING_MAX_HEIGHT;
-    private static float OBJECT_MIN_HEIGHT;
-    private float fuelTimer = 1f;
-    private float buildingTimer = 0f;
+    private static float FUEL_MIN_HEIGHT;
+    private static float BUILDING_MIN_HEIGHT;
+    private float fuelTimer = 3f;
+    private float buildingTimer = 1.5f;
 
     private Game game;
 
     private Random random;
 
     public ObjectSpawn(Game game) {
-        FUEL_MAX_HEIGHT = HeliGame.VIEW_HEIGHT - 200f;
-        BUILDING_MAX_HEIGHT = HeliGame.VIEW_HEIGHT - 250f;
-        OBJECT_MIN_HEIGHT = 75f;
+        FUEL_MAX_HEIGHT = HeliGame.VIEW_HEIGHT - 250f;
+        BUILDING_MAX_HEIGHT = HeliGame.VIEW_HEIGHT - 200f;
+        FUEL_MIN_HEIGHT = 50f;
+        BUILDING_MIN_HEIGHT = 100f;
         this.game = game;
         random = new Random();
     }
@@ -47,13 +49,13 @@ public class ObjectSpawn {
             Building building = null;
             float max_height = BUILDING_MAX_HEIGHT;
             try {
-                building = new Building(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH, random.nextFloat() * max_height + OBJECT_MIN_HEIGHT - 780));
+                building = new Building(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH - 200f, random.nextFloat() * max_height + BUILDING_MIN_HEIGHT - 780f));
             } catch (OccupiedPositionError occupiedPositionError) {
                 max_height = occupiedPositionError.getCollidingObject().getPosition().y;
                 Gdx.app.log(TAG,"Added building colliding, new max height " + max_height);
 
                 try {
-                    building = new Building(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH, random.nextFloat() * max_height + OBJECT_MIN_HEIGHT - 780));
+                    building = new Building(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH - 200f, random.nextFloat() * max_height + BUILDING_MIN_HEIGHT - 780f));
                 } catch (OccupiedPositionError occupiedPositionError2) {
                     occupiedPositionError2.printStackTrace();
                     System.exit(0);
@@ -70,15 +72,15 @@ public class ObjectSpawn {
         while(fuelTimer >= FUEL_SPAWN_TIME) {
             fuelTimer -= FUEL_SPAWN_TIME;
             Item refuel = null;
-            float min_height = OBJECT_MIN_HEIGHT;
+            float min_height = FUEL_MIN_HEIGHT;
             try {
-                refuel = new Refuel(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH, random.nextFloat() * FUEL_MAX_HEIGHT + min_height));
+                refuel = new Refuel(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH - 200f, random.nextFloat() * FUEL_MAX_HEIGHT + min_height));
             } catch (OccupiedPositionError occupiedPositionError) {
                 min_height = occupiedPositionError.getCollidingObject().getCollision().getBoundingRectangle().getHeight() + occupiedPositionError.getCollidingObject().getPosition().y;
                 Gdx.app.log(TAG,"Added item colliding, new min height " + min_height);
 
                 try {
-                    refuel = new Refuel(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH, random.nextFloat() * FUEL_MAX_HEIGHT + min_height));
+                    refuel = new Refuel(new Vector2(helicopter.getPosition().x + HeliGame.VIEW_WIDTH - 200f, random.nextFloat() * FUEL_MAX_HEIGHT + min_height));
                 } catch (OccupiedPositionError occupiedPositionError2) {
                     occupiedPositionError2.printStackTrace();
                     System.exit(0);
